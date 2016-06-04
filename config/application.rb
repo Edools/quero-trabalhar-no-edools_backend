571ea3f816a58905b5a34f903490edda54c95943
Fernando Varani <fervarani@gmail.com>
2016-06-04 11:04:53 -0300

    Use factory girl as default / set up db

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module EdoolsBackendFernandoVarani
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.fixture_replacement :factory_girl
+    end
+    
   end
 end
