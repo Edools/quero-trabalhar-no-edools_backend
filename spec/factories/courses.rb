140606fc6e8b4e9e5e041a22b655e237c5fd200b
Fernando Varani <fervarani@gmail.com>
2016-06-04 14:01:40 -0300

    Change some courses factory

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -2,13 +2,13 @@ require 'faker'
 
 FactoryGirl.define do
   factory :course do
-    title Faker::StarWars.name
+    title Faker::StarWars.character
     description Faker::StarWars.quote
     content Faker::StarWars.quote
     duration "#{Faker::Number.between(4,10)} semestres"
     creation_date Faker::Time.between(2.years.ago, 1.year.ago, :all)
     active_students Faker::Number.between(1000,5000)
-    price Faker::Number.between(100,500)
+    price Faker::Number.between(100.00,500.00).round(2)
   end
 
 end
