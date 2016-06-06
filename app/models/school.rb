3776c2262e84f7a9012057f2a5959a2f8f39c731
Fernando Varani <fervarani@gmail.com>
2016-06-05 22:48:14 -0300

    Added simple subdomain regex validation

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,6 +3,7 @@ class School < ActiveRecord::Base
   
   has_many :courses
   validates_presence_of :name, :owner_email, :pitch, :subdomain, :date_of_creation
+  validates_format_of :subdomain, with: /\A^[-a-z]+$\z/
 
   def active_students
     self.courses.pluck(:active_students).sum if self.courses.any?
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -16,6 +16,18 @@ RSpec.describe School, type: :model do
     expect(school).to validate_presence_of(:date_of_creation)
   end
 
+  it "has subdomain validation" do 
+    school.subdomain = "nice-subdomain"
+
+    expect(school).to be_valid
+  end
+
+  it "has subdomain validation for invalid inputs" do 
+    school.subdomain = "bad.subdomain*"
+
+    expect(school).to be_invalid
+  end
+
   it { should have_many(:courses) }
 
 end
