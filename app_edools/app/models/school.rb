aae6615b94ed2cd36f2ddf018972631c9c425439
Junior <nog.junior84@gmail.com>
2016-06-07 17:56:37 -0300

    Validation form model school

--- app_edools/app/models/school.rb
+++ app_edools/app/models/school.rb
@@ -1,2 +1,6 @@
 class School < ActiveRecord::Base
+  validates :name, :email_responsible, :pitch, presence: true
+  validates_uniqueness_of :name, :email_responsible, :pitch, :subdomain
+  validates_format_of :subdomain, with: /[A-z\d]*.[A-z\d]*\.edools.com/
+  validates_format_of :email_responsible, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
 end
