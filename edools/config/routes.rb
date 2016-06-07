f6168be24746b237da5f80260c5c174ff3b26e3b
Willian <willian_ig3@hotmail.com>
2016-06-07 09:55:39 -0300

    Add resources.

--- edools/config/routes.rb
+++ edools/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  resources :escolas, :cursos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
