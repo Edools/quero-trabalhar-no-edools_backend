20e62b7d6722ce0241511d5d9270f706fa600540
Junior <nog.junior84@gmail.com>
2016-06-07 17:40:37 -0300

    implementation of the action show

--- app_edools/app/controllers/schools_controller.rb
+++ app_edools/app/controllers/schools_controller.rb
@@ -1,4 +1,6 @@
 class SchoolsController < ApplicationController
+  before_action :find_school, only: [:show]
+
   def index
     @schools = School.all.order(name: :asc)
   end
@@ -6,4 +8,26 @@ class SchoolsController < ApplicationController
   def new
     @school = School.new
   end
+
+  def create
+    @school = School.create(school_params)
+    if @school.save
+      redirect_to @school
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+  def school_params
+    params.require(:school).permit(:name, :email_responsible, :pitch, :subdomain)
+  end
+
+  def find_school
+    @school = School.find(params[:id])
+  end
+
 end
