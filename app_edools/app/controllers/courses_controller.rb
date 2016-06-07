39cbcacdcddb86489393a11915005bef95030a57
Junior <nog.junior84@gmail.com>
2016-06-07 19:42:29 -0300

    implementation of the action edit, update and destroy course

--- app_edools/app/controllers/courses_controller.rb
+++ app_edools/app/controllers/courses_controller.rb
@@ -1,4 +1,5 @@
 class CoursesController < ApplicationController
+  before_action :find_school_course, only: [:show, :edit, :update, :destroy]
 
   def new
     @course = Course.new
@@ -9,5 +10,31 @@ class CoursesController < ApplicationController
     @course = @school.courses.create!(params[:course].permit(:title, :description, :content, :duration, :created_at, :price))
 
     redirect_to school_path(@school)
+  end
+
+  def edit
+  end
+
+  def update
+    if @course.update(course_params)
+      redirect_to school_path(@school)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to school_path(@school)
+  end
+
+  private
+  def course_params
+    @course = params.require(:course).permit(:title, :description, :content, :duration, :created_at, :price)
+  end
+
+  def find_school_course
+    @course = Course.find(params[:id])
+    @school = @course.school_id
   end
 end
