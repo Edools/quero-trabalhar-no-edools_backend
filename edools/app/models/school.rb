7a1a343a7d4afa342eacfd2499a693732d7b7950
Willian <w.goncalves91@gmail.com>
2016-06-08 18:33:08 -0300

    Fix validation.

--- edools/app/models/school.rb
+++ edools/app/models/school.rb
@@ -2,7 +2,7 @@ class School < ActiveRecord::Base
   has_many :students
   has_many :courses
 
-  validates :name, presence: true, uniqueness: true,
+  validates :name, presence: true, uniqueness: true
   #validates :owner_email gem install validates_email_format_of
   validates :pitch, presence: true
 end
