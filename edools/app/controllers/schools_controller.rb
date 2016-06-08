6ad8e843bbf421fe742cf5bf96e6e653be176ce6
Willian <w.goncalves91@gmail.com>
2016-06-08 18:33:35 -0300

    Implement basic funcionality.

--- edools/app/controllers/schools_controller.rb
+++ edools/app/controllers/schools_controller.rb
@@ -1,9 +1,26 @@
 class SchoolsController < ApplicationController
 
+    def index
+      @schools = School.all
+    end
+
+    def show
+      @school = School.find(params[:id])
+    end
+    
     def new
     end
 
-    def create        
+
+    def create
+      @school = School.new(school_params)
+      @school.save
+      redirect_to @school
     end
+
+    private
+      def school_params
+        params.require(:school).permit(:name, :owner_email, :pitch, :subdomain)
+      end
 
 end
