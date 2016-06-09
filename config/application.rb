d795551a4494476b4b34db8be7f49a6470ea0425
Thiago F. Lins <thiago.evandson@gmail.com>
2016-06-08 23:33:54 -0300

    Added a Semantic UI frontend framework

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
 
 module QueroTrabalharNoEdoolsBackend
   class Application < Rails::Application
