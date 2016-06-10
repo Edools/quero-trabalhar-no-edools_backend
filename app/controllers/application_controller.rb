f87f9d0cef211b830ed8442b2000f5ef6a3a5442
Thiago F. Lins <thiago.evandson@gmail.com>
2016-06-10 16:07:22 -0300

    customizing devise layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authenticate_user!
+
+  layout :by_resource
+
+  protected
+
+  def by_resource
+    if devise_controller?
+      'auth'
+    else
+      'application'
+    end
+  end
 end
