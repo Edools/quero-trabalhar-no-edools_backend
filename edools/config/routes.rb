e570171cd6c0438167f615d94997939d59aba15d
Willian <w.goncalves91@gmail.com>
2016-06-10 00:33:24 -0300

    Change root to school#index.

--- edools/config/routes.rb
+++ edools/config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'schools#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
