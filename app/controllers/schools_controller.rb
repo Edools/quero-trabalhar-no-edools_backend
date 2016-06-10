4fb9abc2a79a7702c8e23a37972f2089dcac9581
Nathan Souza <nathanpsouza@gmail.com>
2016-06-10 14:27:48 -0300

    created controller for schools crud, rspec tests, added empty views, routes and haml-rails gem

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,50 @@
+class SchoolsController < ApplicationController
+  before_action :load_school, except: [:index, :new, :create]
+
+  def index
+    @schools = School.all
+  end
+
+  def new
+    @school = School.new
+  end
+
+  def create
+    @school = School.new(school_params)
+    if @school.save
+      redirect_to school_path(@school)
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @school.update_attributes(school_params)
+      redirect_to school_path(@school)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @school.destroy
+    redirect_to schools_path
+  end
+
+  private
+  def school_params
+    params.require(:school).permit(:name, :contact_email, :pitch, :subdomain)
+  end
+
+  def load_school
+    @school = School.find(params[:id])
+  end
+end
