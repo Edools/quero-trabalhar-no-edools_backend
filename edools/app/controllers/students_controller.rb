b9840b3e8b0422939b7ba9b9b0f3977291c615b6
Willian <w.goncalves91@gmail.com>
2016-06-09 22:29:37 -0300

    Implement student tests.

--- edools/app/controllers/students_controller.rb
+++ edools/app/controllers/students_controller.rb
@@ -1,12 +1,48 @@
 class StudentsController < ApplicationController
 
+  #index, show, new, edit, create, update, destroy
+
     def index
+      @students = Student.all
+    end
+
+    def show
+      @student = Student.find(params[:id])
     end
 
     def new
     end
 
-    def create
+    def edit
+      @student = Student.find(params[:id])
     end
+
+    def create
+      @school = School.find(params[:school_id])
+      @student = @school.students.create(student_params)
+      redirect_to school_path(@school)
+    end
+
+    def update
+      @school = School.find(params[:school_id])
+      @student = Student.find(params[:id])
+      if @student.update(student_params)
+        redirect_to school_path(@school)
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @school = School.find(params[:school_id])
+      @student = @school.students.find(params[:id])
+      @student.destroy
+      redirect_to school_path(@school)
+    end
+
+    private
+      def student_params
+        params.require(:student).permit(:name)
+      end
 
 end
--- edools/spec/controllers/students_controller_spec.rb
+++ edools/spec/controllers/students_controller_spec.rb
@@ -2,4 +2,68 @@ require 'rails_helper'
 
 RSpec.describe StudentsController, type: :controller do
 
+  before(:all) do
+    DatabaseCleaner.start
+    @school = create(:school)
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  describe "GET index", :type => :controller do
+    it "renders the index template" do
+      get :index, {school_id: @school.id}
+      expect(response).to be_success
+      expect(response).to render_template(:index)
+    end
+
+    it "assigns @courses" do
+      get :index, {school_id: @school.id}
+      expect(assigns(:students)).to be_empty
+    end
+  end
+
+  describe "Student CRUD", :type => :controller do
+    it "creates a student and then destroys it" do
+      get :new, {school_id: @school.id}
+      expect(response).to be_success
+      expect(response).to render_template(:new)
+
+      post :create, {school_id: @school.id, student: attributes_for(:student, school_id: @school.id)}
+      expect(Student.count).to eq(1)
+      expect(@school.reload.students.size).to eq(1)
+      expect(response).to redirect_to(school_path(@school.id))
+
+      @student = @school.students.first
+
+      delete :destroy, {school_id: @school.id, id: @student.id}
+      expect(Student.count).to eq(0)
+      expect(@school.reload.students.size).to eq(0)
+      expect(response).to redirect_to(school_path(@school.id))
+    end
+
+    it "renders the student's page" do
+      @student = create(:student)
+
+      get :show, {school_id: @school.id, id: @student.id}
+      expect(response).to be_success
+      expect(assigns(:student)).to eq(@student)
+      expect(response).to render_template(:show)
+    end
+
+    it "renders the student's editing page and updates the student" do
+      @student = create(:student)
+
+      get :edit, {school_id: @school.id, id: @student.id}
+      expect(response).to be_success
+      expect(assigns(:student)).to eq(@student)
+      expect(response).to render_template(:edit)
+
+      patch :update, {school_id: @school.id, id: @student.id, student: attributes_for(:student, name: "updated", school_id: @school.id)}
+      expect(@student.reload.name).to eq("updated")
+      expect(response).to redirect_to(school_path(@school.id))
+    end
+  end
+
 end
