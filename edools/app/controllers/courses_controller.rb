36b0ae845c0c92d1ecf39d4f21b5f69b22e2a384
ricardosilva13 <ricardo.b.silva13@gmail.com>
2016-06-09 23:33:59 -0300

    remove comments in controllers

--- edools/app/controllers/courses_controller.rb
+++ edools/app/controllers/courses_controller.rb
@@ -1,28 +1,22 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
 
-  # GET /courses
-  # GET /courses.json
+
   def index
     @courses = Course.all
   end
 
-  # GET /courses/1
-  # GET /courses/1.json
+
   def show
   end
 
-  # GET /courses/new
   def new
     @course = Course.new
   end
 
-  # GET /courses/1/edit
   def edit
   end
 
-  # POST /courses
-  # POST /courses.json
   def create
     @course = Course.new(course_params)
 
@@ -34,8 +28,6 @@ class CoursesController < ApplicationController
 
   end
 
-  # PATCH/PUT /courses/1
-  # PATCH/PUT /courses/1.json
   def update
 
       if @course.update(course_params)
@@ -46,20 +38,16 @@ class CoursesController < ApplicationController
 
   end
 
-  # DELETE /courses/1
-  # DELETE /courses/1.json
   def destroy
     @course.destroy
     redirect_to courses_url, notice: 'Course was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_course
       @course = Course.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
       params.require(:course).permit(:title, :description, :content, :duration, :date_of_creation, :price, :school_id)
     end
--- edools/app/controllers/students_controller.rb
+++ edools/app/controllers/students_controller.rb
@@ -1,28 +1,22 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :edit, :update, :destroy]
 
-  # GET /students
-  # GET /students.json
+
   def index
     @students = Student.all
   end
 
-  # GET /students/1
-  # GET /students/1.json
+
   def show
   end
 
-  # GET /students/new
   def new
     @student = Student.new
   end
 
-  # GET /students/1/edit
   def edit
   end
 
-  # POST /students
-  # POST /students.json
   def create
     @student = Student.new(student_params)
 
@@ -34,8 +28,6 @@ class StudentsController < ApplicationController
 
   end
 
-  # PATCH/PUT /students/1
-  # PATCH/PUT /students/1.json
   def update
       if @student.update(student_params)
         redirect_to @student, notice: 'Student was successfully updated.'
@@ -44,20 +36,16 @@ class StudentsController < ApplicationController
       end
   end
 
-  # DELETE /students/1
-  # DELETE /students/1.json
   def destroy
     @student.destroy
     redirect_to students_url, notice: 'Student was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_student
       @student = Student.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
       params.require(:student).permit(:name, :enrollment, :status)
     end
