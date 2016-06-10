65460da6f491681708cb04d57aa1ffcca890b6e6
Nathan Souza <nathanpsouza@gmail.com>
2016-06-10 17:22:45 -0300

    integration tests for delete and see details of school

--- spec/features/schools/delete_school_spec.rb
+++ spec/features/schools/delete_school_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 feature 'Delete school' do
   scenario 'Remove school from database' do
+    school = FactoryGirl.create(:school)
+    visit root_path
+    click_link 'Escolas'
+    click_link 'Ver todas'
 
+    within "#school_#{school.id}" do
+      click_link 'Deletar'
+    end
+
+    expect(page).to_not have_content(school.name)
   end
 end
