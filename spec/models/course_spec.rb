588213bd9d5d68a1726c717de0954c8dfceeb008
Thiago F. Lins <thiago.evandson@gmail.com>
2016-06-10 14:02:17 -0300

    School and Course Model tests

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Course do
+  it { is_expected.to belong_to :school }
 end
