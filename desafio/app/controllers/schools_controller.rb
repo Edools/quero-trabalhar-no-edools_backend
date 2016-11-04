cf6f6b374093c299ec05ad9fb87a3bf998c5f0cd
Gleydson Tavares <tavares.gleydson@gmail.com>
2016-11-04 09:47:05 -0300

    criado action create school

--- desafio/app/controllers/schools_controller.rb
+++ desafio/app/controllers/schools_controller.rb
@@ -2,4 +2,20 @@ class SchoolsController < ApplicationController
   def new
     @school = School.new
   end
+
+  def create
+    @school = School.new(school_params)
+    if @school.save
+      redirect_to @school, notice: "Escola cadastrada com sucesso."
+    else
+      flash[:alert] = "Erro ao cadastrar a escola."
+      render "new"
+    end
+  end
+
+  private
+
+  def school_params
+    params.require(:school).permit(:nome,:email_dono,:pitch,:sub_dominio,:data_criacao)
+  end
 end
