8b76aecf7714055c344556fd0f1befd893107acb
Dennis Alves <dennis.alves@live.com>
2016-11-05 18:54:25 -0200

    adding some views

--- edools/app/controllers/courses_controller.rb
+++ edools/app/controllers/courses_controller.rb
@@ -1,13 +1,20 @@
 class CoursesController < ApplicationController
-    def new
-        @course = @School.find(params[:school_id]).courses.build 
+  def new
+    @course = School.find(params[:school_id]).courses.build 
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to @course
+    else
+      render :new
     end
+  end
 
-    def create
-
+  private
+    def course_params
+      params.require(:course).permit(:title, :school, :description, :content, :duration, :price)
     end
-
-    private
-        def course_params
-        end
 end
