c01adbcf26db00bed314ae53f22e8586b1aaf8ab
Gleydson Tavares <tavares.gleydson@gmail.com>
2016-11-05 10:42:36 -0300

    criado teste do modelo escola

--- desafio/app/models/school.rb
+++ desafio/app/models/school.rb
@@ -1,3 +1,7 @@
 class School < ApplicationRecord
-  validates_length_of :nome, minimum: 1, maximum: 50, allow_blank: false
+  validates_length_of :nome, minimum: 5, maximum: 50, allow_blank: false
+  validates_uniqueness_of :nome, :email_dono, :sub_dominio
+  validates :nome, :email_dono, :pitch, :sub_dominio, :data_criacao, presence: true
+  validates_format_of :sub_dominio, :with => /\A[a-zA-Z0-9\-]*?\Z/,:message => 'Aceita apenas letras, número e hífen'
+  validates_format_of :email_dono, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
--- desafio/spec/models/school_spec.rb
+++ desafio/spec/models/school_spec.rb
@@ -2,16 +2,78 @@ require 'rails_helper'
 
 RSpec.describe School, type: :model do
 
-  before do
-    @school = School.new(nome:"Escola Edool",email_dono: "teste@edool.com.br",
-    pitch:"Lorem ipsum dolor sit amet, quo eu soluta detracto, vim eu tation deleniti fabellas, falli prompta",
-    sub_dominio:"escola-edool",data_criacao:Time.now)
-  end
+  subject {
+    described_class.new(nome: "Escola Edools", email_dono: "email@edools.com",
+    pitch:"Lorem ipsum dolor sit amet, quo eu soluta detracto",
+    sub_dominio:"escola-edools", data_criacao:Time.now)
+  }
 
-  describe "quando o nome da escola não foi informado" do
-    before { @school.nome = ""}
-    it { should_not be_valid }
-  end
+  describe "Validações" do
 
+    it "Escola válida" do
+      expect(subject).to be_valid
+    end
+
+    it "quando o nome da escola não foi informado" do
+      subject.nome = ""
+      expect(subject).to_not be_valid
+    end
+
+    it "quando o nome da escola for muito pequeno" do
+      subject.nome = "Esc"
+      expect(subject).to_not be_valid
+    end
+
+    it "quando o nome da escola for muito grande" do
+      subject.nome = "n" * 51
+      expect(subject).to_not be_valid
+    end
+
+    it "quando o E-mail não foi informado" do
+      subject.email_dono = ""
+      expect(subject).to_not be_valid
+    end
+
+    it "quando o pitch não foi informado" do
+      subject.pitch = ""
+      expect(subject).to_not be_valid
+    end
+
+    it "quando o sub_dominio não foi informado" do
+      subject.sub_dominio = ""
+      expect(subject).to_not be_valid
+    end
+
+    it "quando a data de criação não foi informado" do
+      subject.data_criacao = ""
+      expect(subject).to_not be_valid
+    end
+
+    it "quando o subdomimio é válido" do
+      subject.sub_dominio = "academia-bizstart"
+      expect(subject).to be_valid
+    end
+
+    it "quando o subdomimio não é válido" do
+      subject.sub_dominio = "academia_bizstart"
+      expect(subject).to_not be_valid
+    end
+
+    it "quando o E-mail do dono é válido" do
+      subject.email_dono = "emaildodono@email.com"
+      expect(subject).to be_valid
+    end
+
+    it "quando o E-mail do dono não é válido" do
+      subject.email_dono = "emaildodono%email.com"
+      expect(subject).to_not be_valid
+    end
+
+    it "quando o E-mail do dono é válido" do
+      subject.email_dono = "emaildodono@email.com"
+      expect(subject).to be_valid
+    end
+
+  end
 
 end
