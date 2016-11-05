aba03a5f02610ec73898c592f5ced0baff202c76
Gleydson Tavares <tavares.gleydson@gmail.com>
2016-11-05 08:44:38 -0300

    criado teste course

--- desafio/spec/views/school/school.html.erb_spec.rb
+++ desafio/spec/views/school/school.html.erb_spec.rb
@@ -8,7 +8,7 @@ feature 'Criando Escola' do
 
     fill_in 'Nome da Escola', with: 'Escola Edool'
     fill_in 'Email do Dono', with: 'teste@edool.com.br'
-    fill_in 'Pitch', with: 'Lorem ipsum dolor sit amet, quo eu soluta detracto, vim eu tation deleniti fabellas, falli prompta '
+    fill_in 'Apresentação', with: 'Lorem ipsum dolor sit amet, quo eu soluta detracto, vim eu tation deleniti fabellas, falli prompta '
     fill_in 'Subdomínio', with: 'escola-edool'
     fill_in 'Data de Criação', with: Time.now
 
