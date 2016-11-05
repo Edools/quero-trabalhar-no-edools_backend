1367476bdee2d58d3657565765347545600fc53d
Junior <nog.junior84@gmail.com>
2016-11-05 14:48:18 -0200

    Foreign Key School to Courses

--- app_edools_novembro/db/schema.rb
+++ app_edools_novembro/db/schema.rb
@@ -11,10 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105012904) do
+ActiveRecord::Schema.define(version: 20161105163808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.text     "content"
+    t.string   "duration"
+    t.float    "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "school_id"
+  end
+
+  add_index "courses", ["school_id"], name: "index_courses_on_school_id", using: :btree
+
+  create_table "schools", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "pitch"
+    t.string   "subdominio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +56,5 @@ ActiveRecord::Schema.define(version: 20161105012904) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "courses", "schools"
 end
