83e7faf3af8ea5ac26959ae1a4dee16a2034d705
Dennis Alves <dennis.alves@live.com>
2016-11-06 14:48:55 -0200

    pushing some changes and adding the validator file

--- edools/app/models/school.rb
+++ edools/app/models/school.rb
@@ -1,7 +1,13 @@
+require_dependency '../validators/subdomain_validator.rb'
+
 class School < ApplicationRecord
+
+
   validates :name, presence: true
   validates :owner_email, presence: true
-  validates :subdomain, presence: true
+  validates :subdomain, :presence => true,
+                        :uniqueness => true,
+                        :subdomain => true
 
   has_many :courses, dependent: :destroy
 end
