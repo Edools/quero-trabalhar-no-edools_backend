a6233c3899d9567ec70f54765ec751e840977ed4
Arthur Rocha <arthurstomp@gmail.com>
2016-11-08 15:26:18 -0300

    Initial specs of school attributes
    Test existance of instance attributes.
    [RSpec Green]

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe School, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    context '#name' do
+      it { expect(create(:school).name).not_to be_nil }
+      it { expect(create(:school).name).to be_a String }
+    end
+    context '#owner_email' do
+      it { expect(create(:school).owner_email).not_to be_nil }
+      it { expect(create(:school).owner_email).to be_a String }
+    end
+    context '#pitch' do
+      it { expect(create(:school).pitch).not_to be_nil }
+      it { expect(create(:school).pitch).to be_a String }
+    end
+    context '#subdomain' do
+      it { expect(create(:school).subdomain).not_to be_nil }
+      it { expect(create(:school).subdomain).to be_a String }
+    end
+    context '#created_at' do
+      it { expect(create(:school).created_at).not_to be_nil }
+    end
+  end
 end
