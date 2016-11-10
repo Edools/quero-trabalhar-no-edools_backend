9ad42c9431c48ee79ea0b3fcede7c096006de11f
Gustavo Sobral <ghsobral@gmail.com>
2016-11-10 16:12:13 -0300

    Improve layout spec
    Include sidebar links validation

--- schools_app/spec/features/site_layout_spec.rb
+++ schools_app/spec/features/site_layout_spec.rb
@@ -1,6 +1,27 @@
 feature 'Site layout', type: :feature do
-   scenario 'Home link' do
+   scenario 'Home links' do
     visit root_path
+
+    # Visit schools index
+    expect(page).to have_link 'Escolas'
+    click_link 'Escolas'
+    expect(current_path).to eq(schools_path)
+
+    # Click navbar home link
     expect(page).to have_link 'Desafio Edools'
+    click_link 'Desafio Edools'
+    expect(current_path).to eq(root_path)
+
+    # Visit courses index
+    expect(page).to have_link 'Cursos'
+    click_link 'Cursos'
+    expect(current_path).to eq(courses_path)
+    visit root_path
+
+    # Visit students index
+    expect(page).to have_link 'Alunos'
+    click_link 'Alunos'
+    expect(current_path).to eq(students_path)
+    visit root_path
   end
 end
