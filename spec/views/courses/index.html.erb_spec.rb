e3f6cf8a4dc7621f85e78b6c9248643cd2efd385
Arthur Rocha <arthurstomp@gmail.com>
2016-11-10 16:56:10 -0300

    Conform to RuboCop

--- spec/views/courses/index.html.erb_spec.rb
+++ spec/views/courses/index.html.erb_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "courses/index", type: :view do
+RSpec.describe 'courses/index', type: :view do
   before(:each) do
     school = create(:school)
     assign(:courses, [
       Course.create!(
-        :title => "Title",
-        :description => "MyText",
-        :content => "MyText",
-        :duration => "Duration",
-        :active_students => "9.99",
-        :price => 2.5,
+        title: 'Title',
+        description: 'MyText',
+        content: 'MyText',
+        duration: 'Duration',
+        active_students: '9.99',
+        price: 2.5,
         school_id: school.id
       ),
       Course.create!(
-        :title => "Title",
-        :description => "MyText",
-        :content => "MyText",
-        :duration => "Duration",
-        :active_students => "9.99",
-        :price => 2.5,
+        title: 'Title',
+        description: 'MyText',
+        content: 'MyText',
+        duration: 'Duration',
+        active_students: '9.99',
+        price: 2.5,
         school_id: school.id
       )
     ])
   end
 
-  it "renders a list of courses" do
+  it 'renders a list of courses' do
     render
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 4
-    assert_select "tr>td", :text => "MyText".to_s, :count => 4
-    assert_select "tr>td", :text => "Duration".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 0
-    assert_select "tr>td", :text => 2.5.to_s, :count => 2
+    assert_select 'tr>td', text: 'Title'.to_s, count: 2
+    assert_select 'tr>td', text: 'MyText'.to_s, count: 4
+    assert_select 'tr>td', text: 'MyText'.to_s, count: 4
+    assert_select 'tr>td', text: 'Duration'.to_s, count: 2
+    assert_select 'tr>td', text: '9.99'.to_s, count: 0
+    assert_select 'tr>td', text: 2.5.to_s, count: 2
   end
 end
--- spec/views/courses/show.html.erb_spec.rb
+++ spec/views/courses/show.html.erb_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "courses/show", type: :view do
+RSpec.describe 'courses/show', type: :view do
   before(:each) do
-    @course = assign(:course, Course.create!(
-      :title => "Title",
-      :description => "MyText",
-      :content => "MyText",
-      :duration => "Duration",
-      :active_students => 9,
-      :price => 2.5
-    ))
+    course = Course.create!(
+      title: 'Title',
+      description: 'MyText',
+      content: 'MyText',
+      duration: 'Duration',
+      active_students: 9,
+      price: 2.5
+    )
+    @course = assign(:course, course)
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/MyText/)
