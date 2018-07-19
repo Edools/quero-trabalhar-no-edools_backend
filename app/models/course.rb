ee000ee596cf9a144e130e494e44067c3eb4cd4b
Denis Policarpo <campos_bra@hotmail.com>
2018-07-18 22:54:48 -0300

    Adding validation on models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,8 @@
 class Course < ApplicationRecord
   belongs_to :school
+  
+  validates_presence_of :title, :duration, :price, :active_student
+  validates_numericality_of :price, :greater_than_or_equal_to => 0
+  validates_numericality_of :active_student, :greater_than_or_equal_to => 0, 
+                            :only_integer => true
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,8 @@
 class School < ApplicationRecord
+  has_many :courses, dependent: :destroy
+
+  validates_presence_of :name, :owner_email, :pitch, :subdomain, :date_creation
+  validates :owner_email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :subdomain, format: { with: /\A([a-z][a-z\d]*(-[a-z\d]+)*|xn--[\-a-z\d]+)\z/i },
+                        uniqueness: true
 end
