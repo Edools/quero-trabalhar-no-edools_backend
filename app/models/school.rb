87d72a0f3c78f2eed6b711cbd3bc070a62a243b8
Rafael Hashimoto <hashimotorafa@gmail.com>
2019-09-25 01:23:55 -0300

    create school views

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,8 +1,10 @@
 class School < ApplicationRecord
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
   has_many :courses
 
   validates_presence_of :name, :owner_email, :subdomain, :creation_date
   validates_uniqueness_of :subdomain
   validates_format_of :subdomain, with: /\A\S*\Z/
-  validates_format_of :owner_email, with: Devise::email_regexp
+  validates_format_of :owner_email, with: EMAIL_REGEX
 end
