303128a2641ca6cc5af7e6ca518b55e0411956e5
Rafael Hashimoto <hashimotorafa@gmail.com>
2019-09-26 01:41:42 -0300

    models translation

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module Herospark
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.i18n.default_locale = 'pt-BR'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
